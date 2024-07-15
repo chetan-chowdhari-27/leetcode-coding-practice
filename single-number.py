01a59906b423578e6438d2a5db51f3163d9290a3
Chetan Chowdhari <124226560+chetan-chowdhari-27@users.noreply.github.com>
2024-07-15 12:53:33 +0530

    Update single-number.py
    without libraries counter

--- single-number.py
+++ single-number.py
@@ -13,3 +13,22 @@ class Solution:
 
 sol= Solution()
 print(sol.singleNumber([4,1,2,1,2]))
+
+
+# without libraries 
+
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        counters = {}
+        for num in nums:
+          if num in counters:
+            counters[num] +=1
+          else:
+            counters[num] =1
+
+        duplicates = [item for item, count in counters.items() if count <= 1]
+        new_list = duplicates[0]
+        return new_list
+
+sol= Solution()
+print(sol.singleNumber([9,1,2,1,2,8,8]))
