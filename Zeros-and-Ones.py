4df9e314022862e04c3a718b124498dbcca46a52
Chetan Chowdhari <124226560+chetan-chowdhari-27@users.noreply.github.com>
2024-11-02 19:38:02 +0530

    Create Zeros-and-Ones.py

---
+++ Zeros-and-Ones.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+n = tuple(map(int, input().split()))
+# for i in n:
+arra_np_zero = np.zeros(n,dtype=int)
+print(arra_np_zero)
+arra_np_one = np.ones(n,dtype=int)
+print(arra_np_one)
+# arra_np_zero = np.zeros(n)
